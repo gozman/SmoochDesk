c57e3c52d8763969898268ee68dcada42e6633c4
mike gozzo <mgozzo@gmail.com>
2016-11-17 16:29:26 -0500

    support for link buttons, carousels...

--- client/views/channel/channel.js
+++ client/views/channel/channel.js
@@ -90,22 +90,124 @@ Template.messageForm.events({
         }
       });
 
-      /*
-      Messages.insert({
-        _channel: _id, // Channel reference.
-        message: value,
-        _userId: Meteor.userId(), // Add userId to each message.
-        timestamp: new Date(), // Add a timestamp to each message.
-        role: 'appMaker'
-      });
-      */
-
       // Restore the autosize value.
       instance.$('textarea').css({height: 37});
       window.scrollTo(0, document.body.scrollHeight);
-
-
     }
     $('article').css({'padding-bottom': $('footer').outerHeight()});
-  }
+  },
+  'click button.btn_link': function(event, instance) {
+    event.preventDefault();
+    console.log("Sending link...");
+
+    var _id = Router.current().params._id;
+    var chan = Channels.findOne({_id: _id});
+    var user = Meteor.user();
+
+    Meteor.call('sendMessage', {
+      msg: "Some great websites:",
+      destUserId: chan.userId,
+      role: "appMaker",
+      name: user.username,
+      email: user.emails[0].address,
+      actions: [{"type": "link", "text": "Google", "uri": "http://google.com"},
+                {"type": "link", "text": "Facebook", "uri": "http://facebook.com"},
+                {"type": "link", "text": "Twitter", "uri": "http://twitter.com"},
+                {"type": "link", "text": "Smooch", "uri": "http://smooch.io"},
+               ],
+
+    }, (err, res) => {
+      if (err) {
+        alert(err);
+      } else {
+        // success!
+      }
+    });
+  },
+  'click button.btn_carousel': function(event, instance) {
+    event.preventDefault();
+    console.log("Sending link...");
+
+    var _id = Router.current().params._id;
+    var chan = Channels.findOne({_id: _id});
+    var user = Meteor.user();
+
+    Meteor.call('sendMessage', {
+      destUserId: chan.userId,
+      role: "appMaker",
+      name: user.username,
+      email: user.emails[0].address,
+      items: [{"title": "Big Hat",
+               "description": "This is a very big hat",
+               "mediaUrl": "http://cdn.shopify.com/s/files/1/1010/8284/products/hat_grande.png?v=1449261629",
+               "actions": [{
+                 "text": "Add to Cart ($24.99)",
+                 "type": "postback",
+                 "payload": "BIG_HAT_ADD_TO_CART"
+               }, {
+                 "text": "Learn More",
+                 "type": "link",
+                 "uri": "http://smoochopify.myshopify.com/products/big-hat"
+               }]
+             },
+             {"title": "Yellow Top",
+              "description": "This is a plain yellow top",
+              "mediaUrl": "http://cdn.shopify.com/s/files/1/1010/8284/products/shirt7_grande.png?v=1449262512",
+              "actions": [{
+                "text": "Add to Cart ($29.99)",
+                "type": "postback",
+                "payload": "YELLOW_TOP_ADD_TO_CART"
+              }, {
+                "text": "Learn More",
+                "type": "link",
+                "uri": "http://smoochopify.myshopify.com/products/yellow-top"
+              }]
+            },
+            {"title": "Handbag",
+                     "description": "This is a snazzy handbag",
+                     "mediaUrl": "http://cdn.shopify.com/s/files/1/1010/8284/products/222_grande.jpeg?v=1449262676",
+                     "actions": [{
+                       "text": "Add to Cart ($299.99)",
+                       "type": "postback",
+                       "payload": "HANDBAG_ADD_TO_CART"
+                     }, {
+                       "text": "Learn More",
+                       "type": "link",
+                       "uri": "http://smoochopify.myshopify.com/products/handbag"
+                     }]
+            },
+            {"title": "Printed Polo Shirt",
+                     "description": "This is a polo shirt with dots printed on it",
+                     "mediaUrl": "http://cdn.shopify.com/s/files/1/1010/8284/products/shirt6_grande.png?v=1449261846",
+                     "actions": [{
+                       "text": "Add to Cart ($39.95)",
+                       "type": "postback",
+                       "payload": "PRINTED_POLO_ADD_TO_CART"
+                     }, {
+                       "text": "Learn More",
+                       "type": "link",
+                       "uri": "http://smoochopify.myshopify.com/products/printed-polo-shirt"
+                     }]
+                   },
+           {"title": "Bird Dress",
+                    "description": "Put a bird on it 🕊",
+                    "mediaUrl": "http://cdn.shopify.com/s/files/1/1010/8284/products/dress1_grande.png?v=1449261607",
+                    "actions": [{
+                      "text": "Add to Cart ($42.99)",
+                      "type": "postback",
+                      "payload": "BIRD_DRESS_ADD_TO_CART"
+                    }, {
+                      "text": "Learn More",
+                      "type": "link",
+                      "uri": "http://smoochopify.myshopify.com/products/bird-dress"
+                    }]
+                  }]
+    }, (err, res) => {
+      if (err) {
+        alert(err);
+      } else {
+        // success!
+      }
+    });
+  },
 });
